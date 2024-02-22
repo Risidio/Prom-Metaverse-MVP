4eec4576f81bb242fc6f6834d5acef99a678df35
Batiukova Nataliia <111600960+batiukova-nataliia@users.noreply.github.com>
2024-02-22 15:27:06 +0000

    Revert "Added onHover Popup"

--- src/Components/Icons/Question.tsx
+++ src/Components/Icons/Question.tsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const QuestionIcon = () => {
-  const [onHover, setHover] = useState<boolean>(false);
   return (
     <Link to={"/?"}>
-      <div className="question" onMouseEnter={() => setHover(true)} >?</div>
-      {onHover &&
-      <div className="question-wrapper" onMouseLeave={() => setHover(false)}>
-        <div className="question-heading-container">
-          <h2 className="heading-container-primary-heading">You’re on the PROM map</h2>
-        </div>
-        <div className="question-info">
-          <p className="info-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
-      </div>}
+      <div className="question">?</div>
     </Link>
+
   );
 };
+
 export default QuestionIcon;

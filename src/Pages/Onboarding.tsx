1d721087f3727b352da685601c1c870e9f75ef67
louai khodary <louai.z.khodary@gmail.com>
2024-02-28 11:33:33 +0200

    fix

--- src/Pages/Onboarding.tsx
+++ src/Pages/Onboarding.tsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react";
-import QuestionIcon from "../Components/Icons/Question";
-import Navbar from "../Layouts/NavBar";
-import OutgoingCall from "../Layouts/Popup/Call/OutgoingCall";
-import OnBoardingFirst from "../Layouts/Popup/OnboardingPopup/OnboardingFirst";
-import IncomingCall from "../Layouts/Popup/Call/IncomingCall";
-import ActiveCall from "../Layouts/Popup/Call/ActiveCall";
-import { collaboratorsArray, textArray, titleArray, users } from "../utils/arrays/arrays";
-import Pin from "../Components/Icons/Pin";
+import { useEffect, useState } from 'react';
+import QuestionIcon from '../Components/Icons/Question';
+import Navbar from '../Layouts/NavBar';
+import OutgoingCall from '../Layouts/Popup/Call/OutgoingCall';
+import OnBoardingFirst from '../Layouts/Popup/OnboardingPopup/OnboardingFirst';
+import IncomingCall from '../Layouts/Popup/Call/IncomingCall';
+import ActiveCall from '../Layouts/Popup/Call/ActiveCall';
+import {
+  collaboratorsArray,
+  textArray,
+  titleArray,
+  users,
+} from '../utils/arrays/arrays';
+import Pin from '../Components/Icons/Pin';
 
 const OnBoarding = () => {
   const [callVisibility, setCallVisibility] = useState(false);
   const [selectedUser, setSelectedUser] = useState('');
-  const [fetchedCollaboratos, setFetchedCollaborators] = useState(collaboratorsArray);
+  const [fetchedCollaboratos, setFetchedCollaborators] =
+    useState(collaboratorsArray);
   const [noFound, setNoFound] = useState(false);
   const [noFoundUser, setNoFoundUser] = useState(false);
-
 
   const [searchInput, setSearchInput] = useState('');
   const [searchInputUsers, setSearchInputUsers] = useState('');
 
   const handleCallVisibility = () => {
     setCallVisibility(!callVisibility);
-  }
+  };
 
   const handleButtonClick = (userName: string) => {
     setSelectedUser(userName);
   };
-
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
@@ -36,38 +40,37 @@ const OnBoarding = () => {
     setSearchInputUsers(e.target.value);
   };
 
+  const filteredCollaborators = fetchedCollaboratos.filter((collaborator) =>
+    collaborator.userName.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
-
-
-  const filteredCollaborators = fetchedCollaboratos
-    .filter(collaborator => collaborator.userName
-      .toLowerCase()
-      .includes(searchInput.toLowerCase()));
-
-  const filteredUsers = users
-    .filter(user => user.userName
-      .toLowerCase()
-      .includes(searchInputUsers.toLowerCase()));
-
-
+  const filteredUsers = users.filter((user) =>
+    user.userName.toLowerCase().includes(searchInputUsers.toLowerCase())
+  );
 
   const handleAddUser = (userName: string, role: string, status: string) => {
     // Check if the user is not already in the collaboratorsArray
-    if (!collaboratorsArray.some(collaborator => collaborator.userName === userName)) {
+    if (
+      !collaboratorsArray.some(
+        (collaborator) => collaborator.userName === userName
+      )
+    ) {
       // Add a new collaborator with the provided userName, role, and status
       const newUser = {
         userName,
-        role,  // You can provide a default role for the new user
-        status,  // You can provide a default status for the new user
+        role, // You can provide a default role for the new user
+        status, // You can provide a default status for the new user
       };
 
       // Update the collaboratorsArray using the state setter
-      setFetchedCollaborators(prevCollaborators => [...prevCollaborators, newUser]);
+      setFetchedCollaborators((prevCollaborators) => [
+        ...prevCollaborators,
+        newUser,
+      ]);
 
       console.log(1);
     }
   };
-
 
   useEffect(() => {
     if (filteredCollaborators.length > 0) {
@@ -85,58 +88,60 @@ const OnBoarding = () => {
     }
   }, [filteredUsers]);
 
-
   return (
     <main>
-      <div className="onboarding">
-        <div className="logo"></div>
+      <div className='onboarding'>
+        <div className='logo'></div>
         <QuestionIcon></QuestionIcon>
 
         <OnBoardingFirst
           textArray={textArray}
-          titleArray={titleArray}></OnBoardingFirst>
-        <Navbar userName="userName"
-        level={1}
-        contactValue={searchInput}
-        handleInputChange={handleInputChange}
-        handleCallVisibility={handleCallVisibility}
-
-        collaborators={filteredCollaborators}
-        onButtonClick={handleButtonClick}
-        showNoFound={noFound}
-
-        users={filteredUsers}
-        contactValueUser={searchInputUsers} 
-        handleInputChangeUser={handleInputChangeUsers } 
-        onButtonClickAddUser={handleAddUser}
-        showNoFoundUser={noFoundUser}
+          titleArray={titleArray}
+        ></OnBoardingFirst>
+        <Navbar
+          userName='userName'
+          level={1}
+          contactValue={searchInput}
+          handleInputChange={handleInputChange}
+          handleCallVisibility={handleCallVisibility}
+          collaborators={filteredCollaborators}
+          onButtonClick={handleButtonClick}
+          showNoFound={noFound}
+          users={filteredUsers}
+          contactValueUser={searchInputUsers}
+          handleInputChangeUser={handleInputChangeUsers}
+          onButtonClickAddUser={handleAddUser}
+          showNoFoundUser={noFoundUser}
         />
 
-
-        {callVisibility &&
+        {callVisibility && (
           <OutgoingCall
             handleCallVisibility={handleCallVisibility}
-            userName={selectedUser} />
-        }
+            userName={selectedUser}
+          />
+        )}
 
-        {callVisibility &&
+        {callVisibility && (
           <IncomingCall
             handleCallVisibility={handleCallVisibility}
-            userName={selectedUser} />
-        }
+            userName={selectedUser}
+          />
+        )}
 
-
-        {callVisibility &&
+        {callVisibility && (
           <ActiveCall
             handleCallVisibility={handleCallVisibility}
-            userName={selectedUser} callDuration={'10:05'} />
-        }
-
+            userName={selectedUser}
+            callDuration={'10:05'}
+          />
+        )}
       </div>
-      <Pin menuBuildingName="ScriptWriter Building" menuBuildingText="Here you can come read, review and share scripts with the other PROM citizens" />
-
+      <Pin
+        menuBuildingName='ScriptWriter Building'
+        menuBuildingText='Here you can come read, review and share scripts with the other PROM citizens'
+      />
     </main>
   );
-}
+};
 
 export default OnBoarding;
